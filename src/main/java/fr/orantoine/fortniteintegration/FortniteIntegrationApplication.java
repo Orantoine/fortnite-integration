5ed14b1df56b06b0b54703b197c4d76f5ea2f702
Antoine Godeau <antoine.godeau@sfr.fr>
2019-03-16 15:31:56 +0100

    Initialisation du projet

---
+++ src/main/java/fr/orantoine/fortniteintegration/FortniteIntegrationApplication.java
@@ -0,0 +1,13 @@
+package fr.orantoine.fortniteintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortniteIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortniteIntegrationApplication.class, args);
+	}
+
+}
